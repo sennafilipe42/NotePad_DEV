a7a05dbe6036b4b35159794fa18fc1ca523abcec
sennafilipe42 <filipesennati@gmail.com>
2023-06-03 19:14:57 -0300

    Criação da NewNotePad_Activity vazia.

---
+++ app/src/main/java/com/example/notepad_dev/Wallpapers_Activity.java
@@ -0,0 +1,14 @@
+package com.example.notepad_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Wallpapers_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpapers);
+    }
+}
