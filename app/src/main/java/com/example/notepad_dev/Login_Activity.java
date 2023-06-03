349373cb9d7f859056e3f682f6262ad2df9dc857
sennafilipe42 <filipesennati@gmail.com>
2023-06-03 02:54:21 -0300

    Ajuste feito no Login, agora está logando por e-mail cadastrado e também pela conta do Google.

--- app/src/main/java/com/example/notepad_dev/Login_Activity.java
+++ app/src/main/java/com/example/notepad_dev/Login_Activity.java
@@ -2,10 +2,14 @@ package com.example.notepad_dev;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,18 +18,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.notepad_dev.databinding.ActivityLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Objects;
 
 public class Login_Activity extends AppCompatActivity {
     ActivityLoginBinding binding;
-    GoogleSignInClient googleLogin;
+    GoogleSignInClient googleSignInClient;
     private FirebaseAuth mAuth;
 
 
@@ -62,13 +72,21 @@ public class Login_Activity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("61017014612-6e8qboo82kqahj5mkndf3tr6lt92eh5d.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+
         //muda o nome do botão do Login do google
         TextView text_botao_logar_google = (TextView) binding.botaoGoogle.getChildAt(0);
 
         text_botao_logar_google.setText("Fazer login com Google Account");
 
 
-        binding.botaoEntrar.setOnClickListener(v -> {
+        binding.botaoEntrar.setOnClickListener(view12 -> {
                 try {
                     loginUsuarioESenha(
                         binding.editUser.getText().toString(),
@@ -81,6 +99,69 @@ public class Login_Activity extends AppCompatActivity {
             }
         );
 
+        binding.botaoGoogle.setOnClickListener(view2 -> signIn());
+
+        binding.linkCriarConta.setOnClickListener(view3 ->{
+            criarConta();
+            finish();
+        });
+    }
+
+    private void signIn(){
+        Intent intent = googleSignInClient.getSignInIntent();
+        //startActivityForResult(intent,1);
+        abreActivity.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> abreActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK){
+                    Intent intent = result.getData();
+
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(intent);
+                    try {
+                        GoogleSignInAccount conta = task.getResult(ApiException.class);
+                        loginComGoogle(conta.getIdToken());
+
+                    }catch(ApiException exception){
+                        Toast.makeText(getApplicationContext(), "Nenhum usuário Google logado no momento.", Toast.LENGTH_LONG).show();
+                        Log.d("Erro: ",exception.toString());
+                    }
+                }
+            }
+    );
+    private void loginComGoogle(String token){
+        AuthCredential credencial = GoogleAuthProvider.getCredential(token,null);
+
+        mAuth.signInWithCredential(credencial).addOnCompleteListener(this, task -> {
+            if(task.isSuccessful()){
+                Toast.makeText(getApplicationContext(), "Login com Google efetuado com Sucesso!", Toast.LENGTH_LONG).show();
+                abrirHome();
+                finish();
+            }else {
+                Toast.makeText(getApplicationContext(), "Erro ao efetuar Login com Google.", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == 1) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(intent);
+            try {
+                GoogleSignInAccount conta = task.getResult(ApiException.class);
+                loginComGoogle(conta.getIdToken());
+
+            }catch(ApiException exception){
+                Toast.makeText(getApplicationContext(), "Nenhum usuário Google logado no momento.", Toast.LENGTH_LONG).show();
+                Log.d("Erro: ",exception.toString());
+            }
+        }
     }
 
     private void loginUsuarioESenha(String usuario, String senha){
@@ -102,6 +183,10 @@ public class Login_Activity extends AppCompatActivity {
                     }
                 });
 
+    }
+    public void criarConta() {
+        Intent intent = new Intent(getApplicationContext(), CreateAccount_Activity.class );
+        startActivity(intent);
     }
 
     private void abrirHome(){
