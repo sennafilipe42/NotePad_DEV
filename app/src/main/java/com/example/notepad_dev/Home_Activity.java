0a2b0280fcb1f376216206fd918d0826b05d4f5e
sennafilipe42 <filipesennati@gmail.com>
2023-06-03 21:17:44 -0300

    Ajustes Gerais

--- app/src/main/java/com/example/notepad_dev/Home_Activity.java
+++ app/src/main/java/com/example/notepad_dev/Home_Activity.java
@@ -5,38 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
 import com.example.notepad_dev.databinding.ActivityHomeBinding;
-import com.example.notepad_dev.databinding.ActivityLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home_Activity extends AppCompatActivity {
 
     ActivityHomeBinding binding;
-    private FirebaseAuth mAuth;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
-
         View view = binding.getRoot();
-
+        setContentView(R.layout.activity_home);
         setContentView(view);
 
-        binding.botaoSair.setOnClickListener(view1 -> {
+        binding.toolbarHome.botaoLogout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             finish();
             abrirLogin();
         });
+        binding.toolbarHome.botaoWallpapers.setOnClickListener(v -> {
+            finish();
+            abrirWallpapers();
+        });
+
+        binding.toolbarHome.botaoInfoCriadorAPP.setOnClickListener(v -> {
+            finish();
+            abrirInfo();
+        });
+
+        binding.addNotePAD.setOnClickListener(v -> {
+            finish();
+            abrirNewNotePad();
+        });
+
     }
 
-    private void abrirLogin(){
-        Intent intent = new Intent(getApplicationContext(), Login_Activity.class );
+    private void abrirLogin() {
+        Intent intent = new Intent(getApplicationContext(), Login_Activity.class);
+        startActivity(intent);
+    }
+
+    private void abrirWallpapers() {
+        Intent intent = new Intent(getApplicationContext(), Wallpapers_Activity.class);
+        startActivity(intent);
+    }
+
+    private void abrirInfo() {
+        Intent intent = new Intent(getApplicationContext(), Info__Activity.class);
+        startActivity(intent);
+    }
+    private void abrirNewNotePad() {
+        Intent intent = new Intent(getApplicationContext(), NewNotePad_Activity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/notepad_dev/Info__Activity.java
@@ -0,0 +1,14 @@
+package com.example.notepad_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info__Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
