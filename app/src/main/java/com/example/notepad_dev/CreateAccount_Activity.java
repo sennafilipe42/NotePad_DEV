4acceb915183235b305dbf8c85f86c5f093f60ce
sennafilipe42 <filipesennati@gmail.com>
2023-06-15 00:02:47 -0300

    Ajustes botões e icones, APP 100% funcional

--- app/src/main/java/com/example/notepad_dev/CreateAccount_Activity.java
+++ app/src/main/java/com/example/notepad_dev/CreateAccount_Activity.java
@@ -1,19 +1,14 @@
 package com.example.notepad_dev;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.notepad_dev.databinding.ActivityCreateAccountBinding;
-import com.example.notepad_dev.databinding.ActivityLoginBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
